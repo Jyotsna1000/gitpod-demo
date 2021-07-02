f27cb424f6221c4b19c814f05da924dd0de19bf8
Jyotsna1000 <jyotsnasharma1000@gmail.com>
2021-07-02 10:05:53 +0000

    Setting up msg

--- .ignore/go-gitpod.sh
+++ .ignore/go-gitpod.sh
@@ -3,7 +3,7 @@ echo "Locating required resources..."
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $DIR
 
-curl --output guru-shifu.tar.gz "https://guru-shifu-artifacts.s3.ap-south-1.amazonaws.com/Guru-Shifu-Trainee-Output/outputDev/guru-shifu-neev-dev.tar.gz"
+curl -s --output guru-shifu.tar.gz "https://guru-shifu-artifacts.s3.ap-south-1.amazonaws.com/Guru-Shifu-Trainee-Output/outputDev/guru-shifu-neev-dev.tar.gz" 
 tar -xf guru-shifu.tar.gz
 
 touch .env
